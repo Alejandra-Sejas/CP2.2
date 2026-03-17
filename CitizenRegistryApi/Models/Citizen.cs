526db9c6268edff8e63f0c7160408c2a9e50dc2d
Alejandra Sejas <alejandrasejas1@upb.edu>
2026-03-16 23:23:22 -0400

    P2-001 add CSV helper and GET all citizen from file

--- CitizenRegistryApi/Models/Citizen.cs
+++ CitizenRegistryApi/Models/Citizen.cs
@@ -1,8 +1,8 @@
 namespace CitizenRegistryApi.Models; 
 public class Citizen
 {
-    public string Firtsname {get; set;} = string.Empty;
-    public string Lastsname {get; set;} = string.Empty;
+    public string FirstName {get; set;} = string.Empty;
+    public string LastName {get; set;} = string.Empty;
     public string CI {get; set;} = string.Empty;
     public string BloodGroup { get; set; } = string.Empty;
     public string PersonalAsset { get; set; } = string.Empty;
